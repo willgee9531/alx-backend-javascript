c8158ca9ea49d7446f2b6736adf23be2af8f45bd
willgee9531 <willgee9531@gmail.com>
2023-06-29 00:24:46 -0700

    iterating through objects values

--- 0x00-ES6_basic/100-createIteratorObject.js
+++
@@ -1,9 +0,0 @@
-export default function createIteratorObject(report) {
-   let employee = [];
-   for (let [depts, emplys] of Object.entries(report.allEmployees)){
-        for (let emp of emplys){
-            employee.push(emp);
-        }
-   }
-   return employee;
-}
