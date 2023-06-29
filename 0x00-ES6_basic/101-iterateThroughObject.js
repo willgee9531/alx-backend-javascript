492957ae7cd2feb3f1812a2e663a7bb6358dd5d6
willgee9531 <willgee9531@gmail.com>
2023-06-29 01:15:51 -0700

    seperating object values with symbol

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let seperated = '', i = 0;
-  for (const str of reportWithIterator) {
-    if (i == reportWithIterator.length - 1) {
-      seperated += str;
-    }
-    else {
-      seperated += str + ' | ';
-    }
-    i++;
-  }
-  return seperated;
+  return reportWithIterator.join(' | ');
 }
