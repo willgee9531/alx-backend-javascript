bacd40577efd39050b0e904d63ea7b55445d3e6e
willgee9531 <willgee9531@gmail.com>
2023-07-05 07:24:30 -0700

    get student id

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,10 @@
-export default function getListStudentIds() {
+const getListStudentIds = (listStudents) => {
+  let listStudentIds = [];
+  if (!(listStudents instanceof Array)) {
+    return listStudentIds;
+  }
+  listStudentIds = listStudents.map((student) => student.id);
+  return listStudentIds;
+};
 
-}
+export default getListStudentIds;
